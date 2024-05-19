d2423c179ec8d2f6955f66c9f1e35c293aa5f816
Maximum Wilder-Smith <maxwildersmith@gmail.com>
2024-05-19 16:07:01 +0200

    added ros streaming of camera

--- Assets/Components/VRStreamer/Scripts/Streamer.cs
+++ Assets/Components/VRStreamer/Scripts/Streamer.cs
@@ -2,50 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using RosMessageTypes.Sensor;
+using RosMessageTypes.BuiltinInterfaces;
+using RosMessageTypes.Std;
 using Unity.Robotics.ROSTCPConnector;
+using Unity.Robotics.ROSTCPConnector.MessageGeneration;
 
 public class Streamer : MonoBehaviour
 {
     public string topic = "/quest/image";
-    public RenderTexture _renderTexture;
+    public bool enabled = true;
 
+    private RenderTexture _renderTexture;
+    private Texture2D _texture2D;
     private ROSConnection _ros;
     private Camera _camera;
+    private HeaderMsg _header;
+    private static Streamer _instance;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     void Start()
     {
         _ros = ROSConnection.GetOrCreateInstance();
-        _camera = Camera.main;
 
-        _renderTexture = new RenderTexture(_camera.pixelWidth, _camera.pixelHeight, 24);
+        GameObject cameraObject = new GameObject("ROSCamera");
+        _camera = cameraObject.AddComponent<Camera>();
+
+        _camera.transform.SetParent(Camera.main.transform);
+
+        _renderTexture = new RenderTexture(1280, 720, 24);
         _camera.targetTexture = _renderTexture;
-                
+        _camera.transform.localPosition = Vector3.zero;
+        _camera.transform.localRotation = Quaternion.identity;
+        _camera.transform.localScale = Vector3.one;
+
+        _texture2D = new Texture2D(1280, 720, TextureFormat.RGB24, false);
+
+        _camera.CopyFrom(Camera.main);
+
+        _header = new HeaderMsg(0, new TimeMsg(0, 0), "VR");
+
+
+        _camera.targetTexture = _renderTexture;
+
+        _ros.RegisterPublisher<ImageMsg>(topic);
     }
 
     void Update()
     {
-            ImageMsg msg = new ImageMsg();
-            msg.header.frame_id = "base";
-            msg.height = (uint)_camera.pixelHeight;
-            msg.width = (uint)_camera.pixelWidth;
-            msg.encoding = "rgba8";
-            msg.is_bigendian = 0;
-            msg.step = 4 * (uint)_camera.pixelWidth;
-            msg.data = new byte[msg.step * msg.height];
-            // for (int i = 0; i < msg.height; i++)
-            // {
-            //     for (int j = 0; j < msg.width; j++)
-            //     {
-            //         Color color = _camera.GetPixel(j, i);
-            //         int index = 4 * (i * msg.width + j);
-            //         msg.data[index] = (byte)(color.r * 255);
-            //         msg.data[index + 1] = (byte)(color.g * 255);
-            //         msg.data[index + 2] = (byte)(color.b * 255);
-            //         msg.data[index + 3] = (byte)(color.a * 255);
-            //     }
-            // }
+        if (!enabled)
+            return;
 
-            _ros.Send(topic, msg);
-        
+        // copy the RenderTexture to the Texture2D
+        RenderTexture.active = _renderTexture;
+        _texture2D.ReadPixels(new Rect(0, 0, _renderTexture.width, _renderTexture.height), 0, 0);
+        _texture2D.Apply();
+        RenderTexture.active = null;
+
+        ImageMsg msg = _texture2D.ToImageMsg(_header);
+
+        _ros.Send(topic, msg);
     }
 
+    private void OnDestroy()
+    {
+        if (_instance == this)
+        {
+            _instance = null;
+        }
+    }
 }
