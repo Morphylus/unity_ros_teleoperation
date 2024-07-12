6f93c908848b3a661942b55c9fc5e6374179f61a
Maximum Wilder-Smith <maxwildersmith@gmail.com>
2024-07-12 18:52:35 +0200

    model clean ups

--- Assets/AlmaModel/dynaarm_urdf/alma_rsl_urdf/bin/generate_urdf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Generate urdf and store it in a file
-echo "Generating urdf in $(rospack find alma_rsl_urdf)/resources ..."
-rosrun xacro xacro  -o  $(rospack find alma_rsl_urdf)/resources/alma.urdf $(rospack find alma_rsl_urdf)/urdf/alma.urdf.xacro
-
-echo "Done!"
